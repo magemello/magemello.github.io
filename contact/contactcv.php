9e1ab9c1d2030dc823d77a20e5c057f5bfe5da7b
Mario Romano <dnamario@hotmail.com>
2016-03-03 22:58:32 +0000

    mobile fix

--- contact/contactcv.php
+++ contact/contactcv.php
@@ -41,19 +41,13 @@ if(isset($_POST['submitted'])) {
 	// upon no failure errors let's email now!
 	if(!isset($hasError)) {
 		
+
 		$subject = 'New message to '.$siteTitle.' from '.$name;
 		$sendCopy = trim($_POST['sendCopy']);
 		$body = "Name: $name \n\nEmail: $email \n\nMessage: $comments";
 		$headers = 'From: ' .' <'.$email.'>' . "\r\n" . 'Reply-To: ' . $email;
 
 		mail($emailTo, $subject, $body, $headers);
-		
-        //Autorespond
-		$respondSubject = 'Thank you for contacting '.$siteTitle;
-		$respondBody = "Your message to $siteTitle has been delivered! \n\nWe will answer back as soon as possible.";
-		$respondHeaders = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $emailTo;
-		
-		mail($email, $respondSubject, $respondBody, $respondHeaders);
 		
         // set our boolean completion value to TRUE
 		$emailSent = true;
